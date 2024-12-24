a9bcd1e4a403ac4678c38c1d90203667b357da33
Divyansh-Chaubey <chaubeydivyansh91@gmail.com>
2024-12-24 18:18:12 +0530

    update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById('scroll-left').addEventListener('click', function() {
     const container = document.querySelector('.trending-images');
-    const scrollAmount = 200; // Adjust this value to change the scroll amount
+    const scrollAmount = 200; 
     container.scrollBy({
         top: 0,
         left: -scrollAmount,
-        behavior: 'smooth' // Smooth scrolling
+        behavior: 'smooth' 
     });
 });
 
 document.getElementById('scroll-right').addEventListener('click', function() {
     const container = document.querySelector('.trending-images');
-    const scrollAmount = 200; // Adjust this value to change the scroll amount
+    const scrollAmount = 200; 
     container.scrollBy({
         top: 0,
         left: scrollAmount,
-        behavior: 'smooth' // Smooth scrolling
+        behavior: 'smooth' 
     });
 });
